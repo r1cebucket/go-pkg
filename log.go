4f44b66326acaeb361a2eeff4987635d9930fe8e
Ricebucket <88389934+r1cebucket@users.noreply.github.com>
2023-01-10 01:44:07 +0000

    Create log.go

---
+++ log.go
@@ -0,0 +1,109 @@
+package log
+
+import (
+	"strings"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	jack "gopkg.in/natefinch/lumberjack.v2"
+)
+
+// writer is the global logger.
+// archive the log file after a certain amount of data
+var writer *jack.Logger
+
+// Setup config
+func Setup(level string) {
+	writer = &jack.Logger{
+		Filename:   "log.txt",
+		MaxSize:    50, // megabytes
+		MaxBackups: 100,
+		MaxAge:     365,  //days
+		Compress:   true, //disabled by default
+		LocalTime:  true,
+	}
+	log.Logger = log.Output(writer)
+	// set timezone
+	loc, _ := time.LoadLocation("Asia/Chongqing")
+	zerolog.TimestampFunc = func() time.Time {
+		return time.Now().In(loc)
+	}
+	// config log level
+	level = strings.ToLower(level)
+	switch level {
+	case "debug":
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	case "info":
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	case "warn":
+		zerolog.SetGlobalLevel(zerolog.WarnLevel)
+	case "fatal":
+		zerolog.SetGlobalLevel(zerolog.FatalLevel)
+	default:
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	}
+}
+
+func GetWriter() *jack.Logger {
+	return writer
+}
+
+// Err starts a new message with error level with err as a field if not nil or
+// with info level if err is nil.
+//
+// You must call Msg on the returned event in order to send the event.
+func Err(err error) *zerolog.Event {
+	return log.Err(err)
+}
+
+// Trace starts a new message with trace level.
+//
+// You must call Msg on the returned event in order to send the event.
+func Trace() *zerolog.Event {
+	return log.Trace()
+}
+
+// Debug starts a new message with debug level.
+//
+// You must call Msg on the returned event in order to send the event.
+func Debug() *zerolog.Event {
+	return log.Debug()
+}
+
+// Info starts a new message with info level.
+//
+// You must call Msg on the returned event in order to send the event.
+func Info() *zerolog.Event {
+	return log.Info()
+}
+
+// Warn starts a new message with warn level.
+//
+// You must call Msg on the returned event in order to send the event.
+func Warn() *zerolog.Event {
+	return log.Warn()
+}
+
+// Error starts a new message with error level.
+//
+// You must call Msg on the returned event in order to send the event.
+func Error() *zerolog.Event {
+	return log.Error()
+}
+
+// Fatal starts a new message with fatal level. The os.Exit(1) function
+// is called by the Msg method.
+//
+// You must call Msg on the returned event in order to send the event.
+func Fatal() *zerolog.Event {
+	return log.Fatal()
+}
+
+// Panic starts a new message with panic level. The message is also sent
+// to the panic function.
+//
+// You must call Msg on the returned event in order to send the event.
+func Panic() *zerolog.Event {
+	return log.Panic()
+}
